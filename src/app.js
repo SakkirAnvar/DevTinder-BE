78b24df16b6e88acc445ac74e676b530b648d5d2
SakkirAnvar <sakkir.kokkadan33@gmail.com>
2026-08-01 17:20:07 +0530

    Exploring Routes

--- src/app.js
+++ src/app.js
@@ -2,21 +2,10 @@ const express = require("express")
 
 const app = express()
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello hello")
+app.get("/user",(req, res)=>{
+    res.send({firstName: "Renuka", lastName:"Gavrani"})
 })
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from Test")
-})
-
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello hello")
-})
-
-app.use("/",(req,res)=>{
-    res.send("Hello from Server")
-})
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on PORT 3000");    
