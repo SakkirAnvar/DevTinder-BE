5d034d35b186350b2eefb729e7a4207a46253ca1
SakkirAnvar <sakkir.kokkadan33@gmail.com>
2026-08-04 15:17:51 +0530

    Created API /signup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Sakkir",
+    lastName: "Anvar",
+    emailId: "sakkir@anvar.com",
+    password: "Sakkir@123",
+  });
+
+  try {
+    user.save();
+    res.send("User Data Added Successfully");
+  } catch (err) {
+    res.starus(500).send("Failed to add user", err);
+  }
+});
 
 connectDB()
   .then(() => {
