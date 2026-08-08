c92e419cd367cfc2354ea46acefc74933ebd3dfa
SakkirAnvar <sakkir.kokkadan33@gmail.com>
2026-08-08 16:46:31 +0530

    Validation on user model

--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,35 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    lowercase: true,
+    required: true,
+    trim: true
   },
   password: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
   },
   gender: {
     type: String,
+    validate(value){
+      if(!["male","female","others"].includes(value)){
+        throw new Error("Gender data is not valid")
+      }
+    }
   },
+},
+{
+  timestamps: true,
 });
 
 const userModel = mongoose.model("User", userSchema);
