284c58a713d15c8c78890204a3f2901740a70724
SakkirAnvar <sakkir.kokkadan33@gmail.com>
2026-08-08 10:01:54 +0530

    update-user-API

--- src/app.js
+++ src/app.js
@@ -63,6 +63,18 @@ app.get("/feed", async (req, res)=>{
   }
 })
 
+//update the user /user
+app.patch("/user", async (req, res)=>{
+  const userId = req.body.userId
+  const data = req.body
+  try{
+    await User.findByIdAndUpdate({_id : userId}, data)
+    res.send("User updated Successfully")
+  }catch(err){
+    res.status(400).send("Something went wrong")
+  }
+})
+
 connectDB()
   .then(() => {
     console.log("Database Connection Established Successfully....");
