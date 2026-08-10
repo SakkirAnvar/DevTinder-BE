7b217fadade22e18f763162292fadea7978adc74
SakkirAnvar <sakkir.kokkadan33@gmail.com>
2026-08-10 11:18:50 +0530

    API level Data Sanitization

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ const User = require("./models/user");
 
 const app = express();
 
-app.use(express.json())
-
+app.use(express.json());
 
 //signup API
 app.post("/signup", async (req, res) => {
@@ -20,60 +19,69 @@ app.post("/signup", async (req, res) => {
 });
 
 //find one API /user
-app.get("/user", async (req, res)=>{
-  const userEmail = req.body.emailId
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
   console.log(userEmail);
-  
-  const user = await User.findOne({emailId: userEmail})
-  try{
-    if(!user){
-      res.status(404).send("User not found!")
-    }else{
-      res.send(user)
+
+  const user = await User.findOne({ emailId: userEmail });
+  try {
+    if (!user) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send(user);
     }
-  }catch(err){
-    res.send("Something went wrong")
+  } catch (err) {
+    res.send("Something went wrong");
   }
-})
+});
 
 //delete the user /user
-app.delete("/user", async (req, res)=>{
-  const userId = req.body.userId
-  try{
-    const user = await User.findByIdAndDelete(userId)
-    res.send("User deleted Successfully")
-  }catch(err){
-    res.status(400).send("Something went wrong")
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted Successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
-})
-
+});
 
 // find all users - /feed
-app.get("/feed", async (req, res)=>{
-  const userEmail = req.body.emailId
-  const user = await User.find({emailId: userEmail})
-  try{
-    if(!user){
-      res.status(404).send("User not found!")
-    }else{
-      res.send(user)
+app.get("/feed", async (req, res) => {
+  const userEmail = req.body.emailId;
+  const user = await User.find({ emailId: userEmail });
+  try {
+    if (!user) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send(user);
     }
-  }catch(err){
-    res.send("Something went wrong")
+  } catch (err) {
+    res.send("Something went wrong");
   }
-})
+});
 
 //update the user /user
-app.patch("/user", async (req, res)=>{
-  const userId = req.body.userId
-  const data = req.body
-  try{
-    await User.findByIdAndUpdate({_id : userId}, data)
-    res.send("User updated Successfully")
-  }catch(err){
-    res.status(400).send("Something went wrong")
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const ALLOWED_UPDATES = ["photoUrl", "password", "gender", "skills", "age"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k),
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed!");
+    }
+    if (data?.skills?.length > 10) {
+      throw new Error("Skills not allowed more than 10");
+    }
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User updated Successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err);
   }
-})
+});
 
 connectDB()
   .then(() => {
