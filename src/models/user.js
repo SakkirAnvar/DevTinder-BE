4663541d79d04bf5e5ea39db304f2b30612b3c69
SakkirAnvar <sakkir.kokkadan33@gmail.com>
2026-08-11 16:23:22 +0530

    Validator Library for validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,38 +1,56 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Ivalid email address");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password");
+        }
+      },
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a sample About Section",
+    },
+    skills: [String],
   },
-  lastName: {
-    type: String,
+  {
+    timestamps: true,
   },
-  emailId: {
-    type: String,
-    lowercase: true,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-    validate(value){
-      if(!["male","female","others"].includes(value)){
-        throw new Error("Gender data is not valid")
-      }
-    }
-  },
-},
-{
-  timestamps: true,
-});
+);
 
 const userModel = mongoose.model("User", userSchema);
 
