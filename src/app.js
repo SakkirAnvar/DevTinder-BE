0f88437ecb7249c86b66845721d0156a0c3b9c2c
SakkirAnvar <sakkir.kokkadan33@gmail.com>
2026-08-11 16:31:15 +0530

    Validation of data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const { validateSignupData } = require("./utils/validation");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
   try {
+    validateSignupData();
     user.save();
     res.send("User Data Added Successfully");
   } catch (err) {
